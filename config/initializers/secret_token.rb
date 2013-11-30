f0d5bc81e87aa04e184b7d8d787850754a552dcd
Cheryl Crouse <cheryl.crouse@gmail.com>
2013-11-30 11:30:13 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeehead::Application.config.secret_token = 'ec552c256a0ab4849f9db3b0a8344fd3b97e6fb12ea0bf81635696db0aa60756d776063726d75cca50d612c7f15d50343cbf88d224e1473e530e4f9648e48098'
