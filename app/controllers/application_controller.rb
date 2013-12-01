e47bc7e3907d94bf9f57ad6503748eb9ef45ee69
Chad W Pry <chad.pry@gmail.com>
2013-12-01 15:46:20 -0500

    remove extra files and add the puppy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.unauthorized_path
+  end
+
+  def unauthorized
+    render 'shared/unauthorized'
+  end
 end
