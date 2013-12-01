41b0c5c997666f509044bd5e2bdd5e105e5892ec
Chad W Pry <chad.pry@gmail.com>
2013-12-01 15:09:51 -0500

    lots of changes for setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
+
+  has_paper_trail
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def admin?
+    profile == "admin"
+  end
 end
