ab02b0a8e653cc9408b170dfaafc8775a65fad5a
Chad W Pry <chad.pry@gmail.com>
2013-12-01 16:27:27 -0500

    add more heroku precompile fun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,9 +14,6 @@ Coffeehead::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.initialize_on_precompile = false
-
   # Generate digests for assets URLs
   config.assets.digest = true
 
@@ -46,7 +43,7 @@ Coffeehead::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+  #config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
