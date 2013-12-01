0a9f2835ff6b7f509830e4f9eedc7d4f5c74f4de
Cheryl Crouse <cheryl.crouse@gmail.com>
2013-12-01 17:02:10 -0500

    made profile modifiable

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :first_name, :last_name, :profile, :email, :password, :password_confirmation, :remember_me
 
   has_paper_trail
 
